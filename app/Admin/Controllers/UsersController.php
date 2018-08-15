48c79cff8e56968a0472e39f41146ed0d09198a0
RandyChan <1683224363@qq.com>
2018-08-15 03:52:44 +0000

    管理后台 - 用户列表

---
+++ app/Admin/Controllers/UsersController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class UsersController extends Controller
+{
+    use ModelForm;
+
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+
+            // 页面标题
+            $content->header('用户列表');
+            // $content->description('description');
+
+            $content->body($this->grid());
+        });
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param $id
+     * @return Content
+     */
+    public function show($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
+
+            $content->header('Detail');
+            $content->description('description');
+
+            $content->body(Admin::show(User::findOrFail($id), function (Show $show) {
+
+                $show->id();
+
+                $show->created_at();
+                $show->updated_at();
+            }));
+        });
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Admin::grid(User::class, function (Grid $grid) {
+            // 创建一个列名为 ID 的列，内容是用户的 id 字段，并且可以在前端页面点击排序
+            $grid->id('ID')->sortable();
+            // 创建一个列名为 用户名 的列，内容是用户的 name 字段。下面的 email() 和 created_at() 同理
+            $grid->name('用户名');
+            $grid->email('邮箱');
+            $grid->email_verified('已验证邮箱')->display(function ($value) {
+                return $value ? '是' : '否';
+            });
+            $grid->created_at('注册时间');
+            // 不显示创建按钮
+            $grid->disableCreateButton();
+
+            $grid->actions(function ($actions) {
+                // 不在每一行后面展示查看按钮
+                $actions->disableView();
+
+                // 不在每一行后面展示删除按钮
+                $actions->disableDelete();
+
+                // 不在每一行后面展示编辑按钮
+                $actions->disableEdit();
+            });
+
+            $grid->tools(function ($tools) {
+                // 禁用批量删除按钮
+                $tools->batch(function ($batch) {
+                    $batch->disableDelete();
+                });
+            });
+        });
+    }
+}
