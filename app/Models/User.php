9f463f3b889082a9d967a2797ba4eb5ca3db2df0
RandyChan <1683224363@qq.com>
2018-08-15 02:36:43 +0000

    新增收货地址

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+// use App\Models\UserAddress;
 
 class User extends Authenticatable
 {
